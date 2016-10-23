fbdcf65c818c9a8ab9b6a14d1fc3bc273ba9acd1
helinjiang <me@helinjiang.com>
2016-10-23 21:34:07 +0800

    注释掉indexAction，以便使得其能够进入默认的__call方法里面统一处理

--- src/admin/controller/index.js
+++ src/admin/controller/index.js
@@ -7,8 +7,8 @@ export default class extends Base {
    * index action
    * @return {Promise} []
    */
-  indexAction(){
-    //auto render template file index_index.html
-    return this.display();
-  }
+  // indexAction(){
+  //   //auto render template file index_index.html
+  //   return this.display();
+  // }
 }
