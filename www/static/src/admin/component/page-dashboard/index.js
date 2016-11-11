383dd286d4d0c3a1548ba7d5ae6a46b0a0393ffd
helinjiang <me@helinjiang.com>
2016-11-11 23:49:40 +0800

    page-dashboard

---
+++ www/static/src/admin/component/page-dashboard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from 'base';
+
+export default class extends Base {
+  render() {
+    return (
+      <div className="fk-content-wrap">
+        <h2>HELLO, dashboard new</h2>
+      </div>
+    );
+  }
+}
