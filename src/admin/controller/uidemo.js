ede7ae755e8d06be34bdf2e16c523d9ec0dd8ce4
helinjiang <me@helinjiang.com>
2016-11-12 00:03:51 +0800

    page-ui-demo即其子页面的action处理

--- src/admin/controller/uidemo.js
+++ src/admin/controller/uidemo.js
@@ -13,4 +13,18 @@ export default class extends Base {
     console.log('-uidemo indexAction-');
     return this.displayAdminPage();
   }
+
+  test1Action() {
+    //auto render template file index_index.html
+    // return this.display();
+    console.log('-uidemo test1Action-');
+    return this.displayAdminPage();
+  }
+
+  test2Action() {
+    //auto render template file index_index.html
+    // return this.display();
+    console.log('-uidemo test2Action-');
+    return this.displayAdminPage();
+  }
 }
