709ee07abdb436abfd51bedebe917a4a98749246
helinjiang <me@helinjiang.com>
2016-11-15 21:10:16 +0800

    变更模块名字

--- src-client/pages/admin/index.js
+++ src-client/pages/admin/index.js
@@ -6,19 +6,19 @@ import {Router, Route, IndexRedirect, useRouterHistory} from 'react-router';
 import {createHistory} from 'history';
 
 import App from './components/App';
-import Dashboard from './components/page-dashboard';
-import Index from './components/page-index';
+import Dashboard from './components/dashboard';
+import Index from './components/index';
 
-import UIDemo from './components/page-ui-demo';
-import UIDemoIndex from './components/page-ui-demo/page-index';
-import UIDemoTest1 from './components/page-ui-demo/page-test1';
-import UIDemoTest2 from './components/page-ui-demo/page-test2';
+import UIDemo from './components/ui-demo/Container';
+import UIDemoIndex from './components/ui-demo/index';
+import UIDemoTest1 from './components/ui-demo/test1';
+import UIDemoTest2 from './components/ui-demo/test2';
 
 let history = useRouterHistory(createHistory)({
   basename: '/admin',
   queryKey: false
 });
-console.log('nnnnnnnnnnnnnnnnnnnnn')
+
 ReactDOM.render((
     <Router history={history}>
       <Route path="/" component={App}>
