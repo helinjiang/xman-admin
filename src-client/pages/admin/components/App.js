c6988cf8d7b360cbea8cb90b3dc1708944933275
helinjiang <me@helinjiang.com>
2016-11-15 21:58:30 +0800

    尝试引入redux

--- src-client/pages/admin/components/App.js
+++ src-client/pages/admin/components/App.js
@@ -1,28 +1,22 @@
-import React, {PropTypes, Component} from 'react';
+import React from 'react'
+import { Link, browserHistory } from 'react-router'
 
-
-import Sidebar from './sidebar';
-
-class App extends Component {
-  constructor(props, context) {
-    super(props, context);
-    console.log('==app.js== constructor');
-  }
-
-  render() {
-    if (!SysConfig.userInfo.name) {
-      return (
-        <div className="fk">
-          <h1>LOGIN PAGE</h1>
-        </div>);
-    }
-    return (
-      <div className="fk">
-        <Sidebar />
-        {this.props.children}
+export default function App({ children }) {
+  return (
+    <div>
+      <header>
+        Links:
+        {' '}
+        <Link to="/">Home</Link>
+        {' '}
+        <Link to="/foo">Foo</Link>
+        {' '}
+        <Link to="/bar">Bar</Link>
+      </header>
+      <div>
+        <button onClick={() => browserHistory.push('/foo')}>Go to /foo</button>
       </div>
-    );
-  }
+      <div style={{ marginTop: '1.5em' }}>{children}</div>
+    </div>
+  )
 }
-
-export default App;
--- src-client/pages/admin/index.js
+++ src-client/pages/admin/index.js
@@ -1,38 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import 'babel-polyfill';
 
-import {Router, Route, IndexRedirect, useRouterHistory} from 'react-router';
 import {createHistory} from 'history';
 
-import App from './components/App';
-import Dashboard from './components/dashboard';
-import Index from './components/index';
+import {Router, Route, IndexRoute, browserHistory, useRouterHistory} from 'react-router'
 
-import UIDemo from './components/ui-demo/Container';
-import UIDemoIndex from './components/ui-demo/index';
-import UIDemoTest1 from './components/ui-demo/test1';
-import UIDemoTest2 from './components/ui-demo/test2';
+import {syncHistoryWithStore, routerReducer} from 'react-router-redux'
+import store from './store';
+import {DevTools} from './store';
+import {App, Home, Foo, Bar} from './components'
 
-let history = useRouterHistory(createHistory)({
+// 使用 history 组件来扩展，变更路由的相对根目录
+// 如果不需要，则直接使用 react-router 中的 browserHistory 即可
+let browserHistoryByCreate = useRouterHistory(createHistory)({
   basename: '/admin',
   queryKey: false
 });
 
-ReactDOM.render((
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <IndexRedirect to="/index" />
-        <Route path="index" component={Index}/>
-        <Route path="dashboard" component={Dashboard}/>
-        <Route path="uidemo" component={UIDemo}>
-          <IndexRedirect to="/index" />
-          <Route path="index" component={UIDemoIndex} />
-          <Route path="test1" component={UIDemoTest1}/>
-          <Route path="test2" component={UIDemoTest2}/>
+const history = syncHistoryWithStore(browserHistoryByCreate, store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home}/>
+          <Route path="foo" component={Foo}/>
+          <Route path="bar" component={Bar}/>
         </Route>
-      </Route>
-    </Router>
-  ),
+      </Router>
+      <DevTools />
+    </div>
+  </Provider>,
   document.getElementById('app')
-);
+)
+
+// import App from './components/App';
+// import Dashboard from './components/dashboard';
+// import Index from './components/index';
+//
+// import UIDemo from './components/ui-demo/Container';
+// import UIDemoIndex from './components/ui-demo/index';
+// import UIDemoTest1 from './components/ui-demo/test1';
+// import UIDemoTest2 from './components/ui-demo/test2';
+
+// let history = useRouterHistory(createHistory)({
+//   basename: '/admin',
+//   queryKey: false
+// });
+//
+// ReactDOM.render((
+//     <Router history={history}>
+//       <Route path="/" component={App}>
+//         <IndexRedirect to="/index" />
+//         <Route path="index" component={Index}/>
+//         <Route path="dashboard" component={Dashboard}/>
+//         <Route path="uidemo" component={UIDemo}>
+//           <IndexRedirect to="/index" />
+//           <Route path="index" component={UIDemoIndex} />
+//           <Route path="test1" component={UIDemoTest1}/>
+//           <Route path="test2" component={UIDemoTest2}/>
+//         </Route>
+//       </Route>
+//     </Router>
+//   ),
+//   document.getElementById('app')
+// );
