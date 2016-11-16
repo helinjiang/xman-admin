d080bde0d3445dd90c4c1e979bb8271a8f5c65d4
helinjiang <me@helinjiang.com>
2016-11-16 11:31:16 +0800

    标准化写法

--- src-client/pages/admin/components/index/index.js
+++ src-client/pages/admin/components/index/index.js
@@ -1,10 +1,15 @@
 import React, {PropTypes, Component} from 'react';
-import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import {increase, decrease} from '../../actions/count'
 
 class PageIndex extends Component {
+  static propTypes = {
+    number: PropTypes.number.isRequired,
+    increase: PropTypes.func.isRequired,
+    decrease: PropTypes.func.isRequired
+  }
+
   constructor(props, context) {
     super(props, context);
 
@@ -42,47 +47,6 @@ const mapStateToProps = (state) => ({
   number: state.count.number
 });
 
-//将action的所有方法绑定到props上，注意这下面的两种写法是等价的
-// const mapDispatchToProps = (dispatch) => ({
-//   increase: bindActionCreators(increase, dispatch),
-//   decrease: (n) => {
-//     dispatch(decrease(n));
-//   },
-// });
-
-// const mapDispatchToProps = (dispatch) => {
-//   return bindActionCreators({increase, decrease}, dispatch);
-// };
-
-const mapDispatchToProps = (dispatch) => bindActionCreators({increase, decrease}, dispatch);
-
-// const mapDispatchToProps = {increase, decrease};
+const mapDispatchToProps = {increase, decrease};
 
 export default connect(mapStateToProps, mapDispatchToProps)(PageIndex);
-
-// let actions = {
-//   addItem: (text) => {
-//     type: types.ADD_ITEM,
-//       text
-//   }
-// }
-//
-// bindActionCreators(actions, dispatch); // @return {addItem: (text) => dispatch({ type: types.ADD_ITEM, text })}
-
-// bindActionCreators(actionCreators, dispatch)
-// return {actionKey: (...args) => dispatch(actionCreator(...args))}
-
-// function _getPageSize(data) {
-//   return {
-//     type: COMMON_PAGE_SIZE,
-//     data: data
-//   };
-// }
-//
-// export function loadPageSize(forbidHorizontal) {
-//   return (dispatch, getState) => {
-//     let data = getPageSize(forbidHorizontal);
-//
-//     dispatch(_getPageSize(data));
-//   };
-// }
