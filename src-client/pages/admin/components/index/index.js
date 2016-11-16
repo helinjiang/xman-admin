19fe7f11eb4f0cc5ef5b1f17cc6aaaa742c42d10
helinjiang <me@helinjiang.com>
2016-11-16 10:30:17 +0800

    mapDispatchToProps

--- src-client/pages/admin/components/index/index.js
+++ src-client/pages/admin/components/index/index.js
@@ -1,4 +1,5 @@
 import React, {PropTypes, Component} from 'react';
+import {bindActionCreators} from 'redux'
 import {connect} from 'react-redux'
 
 import {increase, decrease} from '../../actions/count'
@@ -12,7 +13,6 @@ class PageIndex extends Component {
   }
 
   handleIncreaseClick() {
-    console.log('--handleIncreaseClick--');
     this.props.increase(1);
   }
 
@@ -41,11 +41,41 @@ const mapStateToProps = (state) => ({
   number: state.count.number
 });
 
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     increase,
-//     decrease
-//   }
-// };
+//将action的所有方法绑定到props上
+const mapDispatchToProps = (dispatch) => ({
+  increase: bindActionCreators(increase, dispatch),
+  decrease: bindActionCreators(decrease, dispatch),
+});
 
-export default connect(mapStateToProps, { increase, decrease })(PageIndex);
+export default connect(mapStateToProps, mapDispatchToProps)(PageIndex);
+
+// 或者直接传递 actionCreator
+// export default connect(mapStateToProps, {increase, decrease})(PageIndex);
+
+
+// let actions = {
+//   addItem: (text) => {
+//     type: types.ADD_ITEM,
+//       text
+//   }
+// }
+//
+// bindActionCreators(actions, dispatch); // @return {addItem: (text) => dispatch({ type: types.ADD_ITEM, text })}
+
+// bindActionCreators(actionCreators, dispatch)
+// return {actionKey: (...args) => dispatch(actionCreator(...args))}
+
+// function _getPageSize(data) {
+//   return {
+//     type: COMMON_PAGE_SIZE,
+//     data: data
+//   };
+// }
+//
+// export function loadPageSize(forbidHorizontal) {
+//   return (dispatch, getState) => {
+//     let data = getPageSize(forbidHorizontal);
+//
+//     dispatch(_getPageSize(data));
+//   };
+// }
