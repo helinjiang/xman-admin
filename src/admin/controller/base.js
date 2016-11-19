524a60bd27a8b73955c07ea3a2e896cef2178909
helinjiang <me@helinjiang.com>
2016-11-19 16:53:38 +0800

    注释

--- src/admin/controller/base.js
+++ src/admin/controller/base.js
@@ -4,40 +4,38 @@ export default class extends think.controller.base {
   /**
    * ThinkJS 支持前置操作，方法名为 __before，该方法会在具体的 Action 调用之前自动调用。
    * 如果前置操作里阻止了后续代码继续执行，则不会调用具体的 Action，这样可以提前结束请求。
+   *
+   * 注意这里的处理方式，由于是单页面应用，用户即使未登录，也正常将页面进行渲染，
+   * 只是将登录信息透传到html页面中，然后由前端页面js来控制展示哪些内容。
+   * 如果登录信息为空，则展示登录框，否则将展示正常的内容。
    */
   async __before() {
     let http = this.http;
-    console.log('----------', http.controller, http.action);
 
-    // 如果某些 action 不用进行处理，则在此直接 return; 即可。
+    // 如果某些 action 不用进行前置操作处理，则在此直接 return; 即可。
     if (http.controller === 'user' && http.action === 'login') {
       return;
     }
 
     // 从 session 中获得用户信息，登录鉴权
     let userInfo = await this.session('userInfo') || {};
-    if (think.isEmpty(userInfo)) {
-      if (this.isAjax()) {
-        return this.fail('NOT_LOGIN');
-      } else {
-        // return this.redirect("/user/login");
-        // console.log('--------------', userInfo);
-        // TODO 测试代码
-        userInfo = {
-          id: 1,
-          name: 'hhh',
-          type: 1
-        };
-      }
-    }
-    console.log('test userInfo', userInfo);
 
-    // 将 userInfo 赋值到模版中可用
+    // userInfo = {
+    //   id: 1,
+    //   name: 'hhh',
+    //   type: 1
+    // };
+
+    // 如果session中不存在用户信息，且当前是 ajax 请求，则直接返回未登录结果即可
+    if (think.isEmpty(userInfo) && this.isAjax()) {
+      return this.fail('NOT_LOGIN');
+    }
+
+    // 设置userInfo，且如果是非 ajax 请求，则将 userInfo 传递到模版中
     this.userInfo = userInfo;
     if (!this.isAjax()) {
       this.assign('userInfo', {id: userInfo.id, name: userInfo.name, type: userInfo.type});
     }
-    // console.log('--__before end--', userInfo)
   }
 
   /**
