5dd05566e0691d6f70b396d2f3d296b9d5638a34
helinjiang <me@helinjiang.com>
2017-02-14 11:28:48 +0800

    新增客户db

--- db/xman.sql
+++ db/xman.sql
@@ -10,10 +10,35 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2017-02-13 20:29:39
+Date: 2017-02-14 11:28:27
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for x_customer
+-- ----------------------------
+DROP TABLE IF EXISTS `x_customer`;
+CREATE TABLE `x_customer` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '姓名',
+  `sex` tinyint(4) NOT NULL DEFAULT '0' COMMENT '0未知 1男 2女',
+  `telephone` char(11) DEFAULT '' COMMENT '电话号码',
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `birthday` date DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT '',
+  `address` varchar(255) DEFAULT '',
+  `status` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1 为有效 -1 为无效',
+  `remark` text,
+  `create_time` datetime NOT NULL,
+  `last_modify_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of x_customer
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for x_user
